3b9e52bd6910defceda463ceaef2d1fb0dd04e22
Sara Devereux <devereux.sara@gmail.com>
2022-09-02 14:41:28 -0400

    signup functionality

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,27 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Password is incorrect", category="error")
+        else:
+            flash("Email does not exist", category="error")
     return render_template("login.html")
 
 @auth.route("/signup", methods=['GET', 'POST'])
@@ -34,8 +46,9 @@ def sign_up():
             flash("Passwords is too short", category="error")
         else:
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method="sha256"))
-            db.dession.add(new_user)
+            db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash("User created")
             return redirect(url_for("views.home"))
 
@@ -45,5 +58,7 @@ def sign_up():
 
 
 @auth.route("/logout")
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for("views.home"))
