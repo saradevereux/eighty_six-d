d3177b097d76834caa4d776d18bd406337c0e995
Sara Devereux <devereux.sara@gmail.com>
2022-09-02 12:49:00 -0400

    hash password

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
+from flask_login import login_user, logout_user, login_required
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
 
@@ -31,7 +33,7 @@ def sign_up():
         elif len(password1) < 6:
             flash("Passwords is too short", category="error")
         else:
-            new_user = User(email=email, username=username, password=password1)
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method="sha256"))
             db.dession.add(new_user)
             db.session.commit()
             flash("User created")
