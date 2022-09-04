f59b34d1314e369bc28da408faf2d411180b5f36
Sara Devereux <devereux.sara@gmail.com>
2022-09-04 14:17:50 -0400

    format

--- website/models.py
+++ website/models.py
@@ -2,6 +2,7 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
@@ -12,23 +13,36 @@ class User(db.Model, UserMixin):
     comments = db.relationship("Comment", backref="user", passive_deletes=True)
     likes = db.relationship("Like", backref="user", passive_deletes=True)
 
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
+    author = db.Column(
+        db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+    )
     comments = db.relationship("Comment", backref="post", passive_deletes=True)
     likes = db.relationship("Like", backref="post", passive_deletes=True)
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey("post.id", ondelete="CASCADE"), nullable=False)
+    author = db.Column(
+        db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+    )
+    post_id = db.Column(
+        db.Integer, db.ForeignKey("post.id", ondelete="CASCADE"), nullable=False
+    )
+
 
 class Like(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey("post.id", ondelete="CASCADE"), nullable=False)
+    author = db.Column(
+        db.Integer, db.ForeignKey("user.id", ondelete="CASCADE"), nullable=False
+    )
+    post_id = db.Column(
+        db.Integer, db.ForeignKey("post.id", ondelete="CASCADE"), nullable=False
+    )
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
 from .models import Post, User, Comment, Like
 from . import db
 
+
 views = Blueprint("views", __name__)
+
 
 @views.route("/")
 @views.route("/home")
@@ -11,6 +13,7 @@ views = Blueprint("views", __name__)
 def home():
     posts = Post.query.all()
     return render_template("home.html", user=current_user, posts=posts)
+
 
 @views.route("/create-post", methods=["GET", "POST"])
 @login_required
@@ -28,6 +31,7 @@ def create_post():
             return redirect(url_for("views.home"))
     return render_template("create_post.html", user=current_user)
 
+
 @views.route("/delete-post/<id>")
 @login_required
 def delete_post(id):
@@ -40,8 +44,9 @@ def delete_post(id):
         db.session.delete(post)
         db.session.commit()
         flash("Post deleted", category="success")
-    
+
     return redirect(url_for("views.home"))
+
 
 @views.route("/posts/<username>")
 @login_required
@@ -52,7 +57,10 @@ def posts(username):
         return redirect(url_for("views.home"))
     posts = user.post
 
-    return render_template("posts.html", user=current_user, posts=posts, username=username)
+    return render_template(
+        "posts.html", user=current_user, posts=posts, username=username
+    )
+
 
 @views.route("create-comment/<post_id>", methods=["POST"])
 @login_required
@@ -71,6 +79,7 @@ def create_comment(post_id):
             flash("Post does not exist.", category="error")
     return redirect(url_for("views.home"))
 
+
 @views.route("delete-comment/<comment_id>")
 @login_required
 def delete_comment(comment_id):
@@ -85,13 +94,14 @@ def delete_comment(comment_id):
 
     return redirect(url_for("views.home"))
 
-@views.route("/like-post/<post_id>", methods=["GET"])
+
+@views.route("/like-post/<post_id>", methods=["POST"])
 @login_required
 def like(post_id):
-    post = Post.query.filter_by(id=post_id)
+    post = Post.query.filter_by(id=post_id).first()
     like = Like.query.filter_by(author=current_user.id, post_id=post_id).first()
     if not post:
-        flash("Post does not exist.", category="error")
+        return jsonify({"error": "Post does not exist."}, 400)
     elif like:
         db.session.delete(like)
         db.session.commit()
@@ -99,4 +109,9 @@ def like(post_id):
         like = Like(author=current_user.id, post_id=post_id)
         db.session.add(like)
         db.session.commit()
-    return redirect(url_for("views.home"))
+    return jsonify(
+        {
+            "likes": len(post.likes),
+            "liked": current_user.id in map(lambda x: x.author, post.likes),
+        }
+    )
